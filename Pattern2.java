334424900e7614f744c51047e56ad4a0d7032334
Aryan Pimpale <aryan.pimpale2007@gmail.com>
2026-05-25 14:45:03 +0530

    Add Pattern 2: Right angle triangle

---
+++ Pattern2.java
@@ -0,0 +1,20 @@
+package DSA;
+import java.util.Scanner;
+
+public class Pattern2 {
+        public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+        System.out.println("Enter the number of rows: ");
+        int n = s.nextInt();
+
+        for(int i = 0; i<n;i++){ //prints rows
+            for(int j = 0; j<=i; j++){  //prints 4 stars(columns)
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+         s.close();
+    }
+}
+

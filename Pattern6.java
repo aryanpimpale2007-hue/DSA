73ca3972f01b44e8401ec0d820dfb741aaa5dc86
Aryan Pimpale <aryan.pimpale2007@gmail.com>
2026-05-27 07:51:17 +0530

    Add Pattern 6: Decreasing number pattern

---
+++ Pattern6.java
@@ -0,0 +1,19 @@
+package DSA;
+import java.util.Scanner;
+
+public class Pattern6 {
+           public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+        System.out.println("Enter the number of rows: ");
+        int n = s.nextInt();
+
+        for(int i = 1; i<=n;i++){ //prints rows
+            for(int j = 1; j<=n-i+1; j++){  //prints 4 stars(columns)
+                System.out.print(" "+j); //prints value of j
+            }
+            System.out.println();
+        }
+         s.close();
+    }
+}

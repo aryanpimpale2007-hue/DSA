288569bac1f2cbdfa90140073d2845a6d1b08401
Aryan Pimpale <aryan.pimpale2007@gmail.com>
2026-05-28 12:28:37 +0530

    Add Pattern 10: Half diamond star pattern

---
+++ Pattern10.java
@@ -0,0 +1,24 @@
+package DSA;
+
+import java.util.*;
+
+public class Pattern10 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter no of rows: ");
+        int n = s.nextInt();
+
+        for(int i=1; i<=2*n-1;i++){
+            int stars = i;
+            if(i>n){
+                stars = 2*n-i;
+            }
+            for(int j =1; j<=stars;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        s.close();
+    }
+}

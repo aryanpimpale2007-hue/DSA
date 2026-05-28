7983d98a9bedae923e089cb013254448afc8f7e5
Aryan Pimpale <aryan.pimpale2007@gmail.com>
2026-05-28 12:23:22 +0530

    Add Pattern 8: Inverted Pyramid star

---
+++ Pattern8.java
@@ -0,0 +1,27 @@
+package DSA;
+
+import java.util.Scanner;
+
+public class Pattern8 {
+               public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+        System.out.println("Enter the number of rows: ");
+        int n = s.nextInt();
+        
+        for(int i = 0; i<n;i++){
+            for(int j = 0; j<i;j++){  //prints space
+                System.out.print(" ");
+            }
+            for(int j = 0; j<2*n-(2*i+1);j++){    // prints stars = 2*n-(2*no of rows+1);
+                System.out.print("*");
+            }
+             for(int j = 0; j<i;j++){   //prints space = total - no of row - 1;
+                System.out.print(" ");
+            }
+              System.out.println();
+        }
+        s.close();
+    }
+}
+

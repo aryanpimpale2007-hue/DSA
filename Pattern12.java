934eb6be55101d06f4495dd57948b95df6a2ea46
Aryan Pimpale <aryan.pimpale2007@gmail.com>
2026-05-29 15:54:43 +0530

    Add Pattern 12: Symmetric number pattern

---
+++ Pattern12.java
@@ -0,0 +1,32 @@
+package DSA;
+import java.util.*;
+
+public class Pattern12 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int n = s.nextInt();
+
+        int space = 2*(n-1);
+
+        for(int i = 1; i<=n;i++){
+            //numbers
+            for(int j = 1; j<=i;j++){
+                System.out.print(j);
+            }
+            //space
+            for(int j = 1; j<=space;j++){
+                System.out.print(" ");
+            }
+            //numbers
+            for(int j = i; j>=1;j--){
+                System.out.print(j);
+            }
+            System.out.println();
+            space-=2;
+        }
+        s.close();
+    }
+}
+

8d734b45920fddcbb2a1fc9fbc8d0f3df009c369
Aryan Pimpale <aryan.pimpale2007@gmail.com>
2026-05-29 15:57:18 +0530

    Add Pattern13: Floyd's triangle pattern

---
+++ Pattern13.java
@@ -0,0 +1,20 @@
+package DSA;
+import java.util.Scanner;
+
+public class Pattern13 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter the number: ");
+        int n = s.nextInt();
+        int num=1;
+        for(int i = 1; i<=n;i++){
+            for(int j = 1; j<=i;j++){
+                System.out.print(num);
+                num = num+1;
+            }
+            System.out.println();
+        }
+        s.close();
+    }
+}

f4b9697cda1c51ffbc52745c0f6447b6d157e79c
Aryan Pimpale <aryan.pimpale2007@gmail.com>
2026-05-31 21:45:33 +0530

    Add Pattern 16: Repeated Alphabet Pattern

---
+++ Pattern16.java
@@ -0,0 +1,21 @@
+package DSA;
+
+import java.util.Scanner;
+
+public class Pattern16{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter the number: ");
+        int n = s.nextInt();
+
+        for(int i=0;i<n;i++){
+            char ch =(char)('A' + i);
+            for(int j = 0; j<=i;j++){
+                System.out.print(ch+" ");
+            }
+            System.out.println();
+        }
+        s.close();
+    }
+}

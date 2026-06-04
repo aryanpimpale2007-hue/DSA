d977610787d4ff832d839297cd456da75fbb29be
Aryan Pimpale <aryan.pimpale2007@gmail.com>
2026-06-04 19:26:07 +0530

    Add prime number checker program

--- Prime_no.java
+++ Prime_no.java
@@ -4,22 +4,25 @@ import java.util.Scanner;
 
 public class Prime_no {
     public static void main(String[] args) {
-         Scanner s = new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
 
-        System.out.println("Enter a number: ");
+        System.out.print("Enter a number: ");
         int n = s.nextInt();
 
-        int count=0;
-        for(int i=1;i<=n;i++){
-            if (n%i==0) {
-                count++;
-            }
-        }           
-        if (count==2) {
-                System.out.println("true");
-            }
-            else System.out.println("False"); 
+        boolean isPrime = true;
 
+        if (n < 2) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i * i <= n; i++) {
+                if (n % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(isPrime);
         s.close();
     }
 }

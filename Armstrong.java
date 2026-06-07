d415caa7182df4236c8870d9b98831a4dab183c0
Aryan Pimpale <aryan.pimpale2007@gmail.com>
2026-06-07 22:59:56 +0530

    Add Armstrong number checker

---
+++ Armstrong.java
@@ -0,0 +1,25 @@
+package DSA;
+import java.util.Scanner;
+
+public class Armstrong {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int n = s.nextInt();
+
+        int dup=n;
+        int sum=0;
+        while (n>0) {
+            int last = n%10;
+            sum = sum+(last*last*last);
+            n=n/10;
+        }
+        if (dup==sum) {
+            System.out.println("It is armstrong");
+        }
+        else System.out.println("It is not armstrong");
+        s.close();
+    }
+
+}
---
+++ Print_Divisors.java
@@ -0,0 +1,19 @@
+package DSA;
+import java.util.Scanner;
+
+public class Print_Divisors {
+    public static void main(String[] args) {
+         Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int n = s.nextInt();
+
+        for(int i=1;i<=n;i++){
+            if (n%i==0) {
+                System.out.println(i);
+            }
+        }        
+            s.close();
+        }
+
+    }

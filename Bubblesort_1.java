7cf3dff3af180fd552c2662f237f5684ac909551
Aryan Pimpale <aryan.pimpale2007@gmail.com>
2026-06-08 11:58:02 +0530

    Optimization of Bubble sort

--- Bubblesort_1.java
+++ Bubblesort_1.java
@@ -14,12 +14,19 @@ public class Bubblesort_1 {
             }
 
         for(int i=n-1;i>=1;i--){   //sorts from 0 to n-1; 0 to n-2; 0 to n-3
+         boolean swapped = false;
             for(int j=0;j<=i-1;j++){ // starts from 0 to n-1, n-2, n-3...
                 if (arr[j]>arr[j+1]) {    //checks which is greater
                     int temp = arr[j+1];  //actual swapping
                     arr[j+1]=arr[j];
                     arr[j]=temp;
+
+                    swapped = true;
                 }
+            }
+
+            if (!swapped) {
+                break;
             }
         }
 

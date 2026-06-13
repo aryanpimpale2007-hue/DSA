5246e9eec06eda347e4c64e384f6df07043e3c3a
Aryan Pimpale <aryan.pimpale2007@gmail.com>
2026-06-13 20:22:44 +0530

    Add second largest optimal solution

---
+++ SecondLargest_array.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class SecondLargest_array {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+         System.out.println("Enter the number of elements: ");
+        int n = s.nextInt();
+
+        int arr[] = new int[n];
+        System.out.println("Enter the numbers: ");
+        for(int i=0;i<n;i++){
+            arr[i]=s.nextInt();             
+            }
+
+        int largest = arr[0];
+        for(int i=0;i<n;i++){
+            if (arr[i]>largest) {
+                largest = arr[i];
+            }
+        }
+        
+        int slargest = -1;
+        for(int i=0; i<n; i++){
+            if (arr[i]<largest && arr[i]>slargest) {
+                slargest=arr[i];
+            }
+        }
+
+        System.out.println("Second largest: "+slargest);
+
+    }
+}

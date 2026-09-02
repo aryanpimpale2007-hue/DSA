8756c19c9d7ed946d085451f14eb7d23e645756b
Aryan Pimpale <aryan.pimpale2007@gmail.com>
2026-09-02 12:01:10 +0530

    Added comments to program

--- Checkarr_sorted.java
+++ Checkarr_sorted.java
@@ -9,17 +9,17 @@ public class Checkarr_sorted {
 
         int arr[] = new int[n];
         System.out.println("Enter the numbers: ");
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){                   //input of numebrs
             arr[i]=s.nextInt();             
             }
 
-        for(int i=1;i<n;i++){
+        for(int i=1;i<n;i++){               //sorting happens here
             if (arr[i]<=arr[i-1]) {
-                System.out.println("False");
+                System.out.println("False");    //if false result stops here
                 return;
             }
         }
-        System.out.println("true");
+        System.out.println("true");         //Shows result
         s.close();
     }
 }
